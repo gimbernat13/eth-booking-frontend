fa38f1e8391e6bf379db726d926f17cea2e81133
Diego Gimbernat <gimbernat13@gmail.com>
2023-01-11 05:49:20 -0500

    🚧 added goerli and connection alert

--- components/atomic/atoms/Layout/Layout.tsx
+++ components/atomic/atoms/Layout/Layout.tsx
@@ -1,10 +1,11 @@
-import {useColorMode} from '@chakra-ui/react';
+import {Alert, AlertIcon, useColorMode} from '@chakra-ui/react';
 import {
   RainbowKitProvider,
   darkTheme as rainbowDarkTheme,
   lightTheme as rainbowLightTheme,
 } from '@rainbow-me/rainbowkit';
 import React from 'react';
+import {useAccount} from 'wagmi';
 
 type Props = {
   children: any;
@@ -13,6 +14,7 @@ type Props = {
 
 export const Layout = ({children, chains}: Props) => {
   const {colorMode} = useColorMode();
+  const {isConnected} = useAccount();
   return (
     <RainbowKitProvider
       theme={colorMode === 'light' ? rainbowLightTheme() : rainbowDarkTheme()}
